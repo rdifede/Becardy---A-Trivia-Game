73731bbd666a542776705042f32a1bc60df62538
Rebecca <rebecca.difede@gmail.com>
2017-06-07 12:53:15 -0400

    categories 2-4 questions and answers added

--- main.js
+++ main.js
@@ -1,29 +1,73 @@
-$(function(){
-console.log("connected");
+$(function() {
+    console.log("connected");
 
-var board = $("#board");
-var correct = [];
-var incorrect = [];
+    var board = $("#board");
+    var correct = [];
+    var incorrect = [];
 
-var categories = {
-	[
-	name: "Potent Potables"
-	questions: [
-		q_one: "First brewed in Mexico in 1897, this beer is best known for its commercials, which feature a rather intriguing, worldly gentleman",
-		q_two: "A 'White Russian' consists of 2 parts vodka, 1 part fresh cream, and 1 part this.",
-		q_three: "Once popular in the 1950's, this 'light liquor alternative', is making a comeback, with people all over Instagram posing with their #spritz"
-		]
-	answers: [
-		a_one: "What is Dos Equis?",
-		a_two: "What is coffee liquer?",
-		a_three: "What is Aperol?"
-		]
-	],
-	[
-	name: ""
+    var categories = {
+        [
+            name: "Potent Potables",
+            questions: [
+                one: "First brewed in Mexico in 1897, this beer is best known for its commercials, which feature a rather intriguing, worldly gentleman",
+                two: "A 'White Russian' consists of 2 parts vodka, 1 part fresh cream, and 1 part this.",
+                three: "Once popular in the 1950's, this 'light liquor alternative', is making a comeback, with people all over Instagram posing with their #spritz"
+            ],
+            answers: [
+                one: "What is Dos Equis?",
+                two: "What is coffee liquer?",
+                three: "What is Aperol?"
+            ]
+        ],
+        [
+            name: "A Galaxy Far Far Away",
+            questions: [
+                one: "'Help me Obi Wan Kenobi, you're my only hope!' is the first line by this royal.",
+                two: "The first planet destroyed by the Death Star.",
+                three: "The foggy, swamp-like place where Yoda lives."
 
-	],
-}
+            ],
+            answers: [
+                one: "Who is Princess Leia?",
+                two: "What is Alderaan?",
+                three: "What is the Degobah system?"
+            ]
+        ],
+
+        [
+            name: "Oh Say Can You 'Sea'",
+            questions: [
+                one: "This bird is commonly found near bodies of water. Don't feed them, or they might attack.",
+                two: "The stitching that binds two pieces of fabric together.",
+                three: "A generic alternative to 'Happy Holidays!'"
+            ],
+            answers: [
+                one: "What is a seagull?";
+                two: "What is the seam?",
+                three: "What is 'Season's Greetings!'?"
+            ]
+
+        ],
+
+        [
+            name: "What a Tool",
+            questions: [
+                one: "Commonly used by Ikea for their build-your-own furniture, it can also be referred to as a hex key.",
+                two: "A staple in most woodshops, this type of sander consists of a revolving strip of abrasive that is used to smooth out and shape the edges of a project",
+                three: "An American brand, primarily known for its rotary tools."
+            ],
+            answers: [
+                one: "What is an allen wrench?",
+                two: "What is a belt sander?",
+                three: "What is Dremel?"
+            ]
+        ],
+
+
+
+
+
+    }
 
 
 
