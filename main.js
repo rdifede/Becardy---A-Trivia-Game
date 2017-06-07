4652c7d60a7460c4021b3869164f01552fa54b26
Rebecca <rebecca.difede@gmail.com>
2017-06-07 11:58:16 -0400

    basic styling layout done and first array of questions declared

---
+++ main.js
@@ -0,0 +1,35 @@
+$(function(){
+console.log("connected");
+
+var board = $("#board");
+var correct = [];
+var incorrect = [];
+
+var categories = {
+	[
+	name: "Potent Potables"
+	questions: [
+		q_one: "First brewed in Mexico in 1897, this beer is best known for its commercials, which feature a rather intriguing, worldly gentleman",
+		q_two: "A 'White Russian' consists of 2 parts vodka, 1 part fresh cream, and 1 part this.",
+		q_three: "Once popular in the 1950's, this 'light liquor alternative', is making a comeback, with people all over Instagram posing with their #spritz"
+		]
+	answers: [
+		a_one: "What is Dos Equis?",
+		a_two: "What is coffee liquer?",
+		a_three: "What is Aperol?"
+		]
+	],
+	[
+	name: ""
+
+	],
+}
+
+
+
+
+
+
+
+
+})
