806125c1c9d4ad191e30edb5786205a9d1f60929
Rebecca <rebecca.difede@gmail.com>
2017-06-09 08:55:03 -0400

    check for answer and check for win logic is written, but theres some bugs

--- main.js
+++ main.js
@@ -124,36 +124,55 @@ $(function() {
 
     var body = $("body");
     var board = $("#board");
+    var modal_container = $(".modal-container");
     var correct = [];
     var incorrect = [];
-    // var counter = $(1);
-
-    // console.log("wut");
 
 
 
 
 
+var checkForWin = function (){
+	var sum = (correct.length + incorrect.length);
+	if (sum === 15){
+		if (correct.length > incorrect.length){
+			var win = $("<div>");
+			win.addClass("modal");
+			win.text("Nice job! You answered " + correct.length + "/15 questions correct! Are you ready for Double Jeopardy?");
+			var dj = $("<button>");
+			dj.attr("a", "#");
+			dj.css("text-decoration", "none");
+			dj.text("Let's Go!");
+			win.append(dj);
+			modal_container.append(win);
+			win.toggle();
+		} else {
+			var lose = $("<div>");
+			lose.addClass("modal");
+			lose.text("Oh no! You only answered " + correct.length +"/15 questions correct. Want to try again?");
+			var tryAgain = $("<button>");
+			tryAgain.attr("a", "./game.html");
+			tryAgain.text("Try Again!");
+			lose.append(tryAgain);
+			modal_container.append(lose);
+			lose.toggle();
+		}
+	} 
+}
 
-// var checkAnswer = function (a){
-// 	a = input.val();
-// 	if (a === input.val()){
-// 		correct.push(a);
-// 	} else if (a !== input.val()){
-// 		incorrect.push(a);
-// 		}
-// 	}
+var checkAnswer = function (q){
+	var input = $("input");
+	if (input.val() === q.a){
+		console.log(input.val());
+		correct.push(q);
+	} else {
+		incorrect.push(q);
+	}
+	console.log(incorrect.length);
+}
 
 
-
-
-
-
-
-
-
-
-var createModal = function(q) {
+var createModal = function(q, c) {
         var modal = $("<div>");
         modal.addClass("modal");
         modal.text(q.q);
@@ -162,6 +181,12 @@ var createModal = function(q) {
         var submit = $("<button>");
         submit.attr("id", "submit");
         submit.text("submit");
+        submit.on("click", function(){
+        	modal.remove();
+        	c.off();
+        	checkAnswer(q);
+        	checkForWin();
+        })
         modal.append(submit);
         
         var input = $("<input>");
@@ -176,31 +201,34 @@ var createModal = function(q) {
         xOut.attr("id", "x");
         xOut.text("x");
         xOut.click(function(){
-        	modal.hide();
+        	modal.remove();
         })
         modal.append(xOut);
 
-        board.append(modal);
+        modal_container.append(modal);
         modal.fadeToggle("fast");
         console.log("clicked");
 
- // checkAnswer();
       console.log(q);  
     }
+
 
 
 
 gameInfo.forEach(function(name){
 	var rowDiv = $("<div>");
 	rowDiv.addClass("category");
-	rowDiv.text(name.category);
+	var title = $("<div>");
+	title.addClass("category_title");
+	title.text(name.category);
+	rowDiv.append(title);
 	board.append(rowDiv);
 		name.questions.forEach(function(question){
 			var cellDiv = $("<div>");
 			cellDiv.addClass("q");
 			cellDiv.text(question.p);
 			cellDiv.on("click",function(){
-				createModal(question);
+				createModal(question,cellDiv);
 			});
 			rowDiv.append(cellDiv);
 		})
